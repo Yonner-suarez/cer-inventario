b92a8e4d198002a0446a687778efe5fc9acfa0ad
Yonner-suarez <118120289+Yonner-suarez@users.noreply.github.com>
2025-10-07 12:23:25 -0500

    limpiando appsetitngs

--- microInventario.API/Util/Variables.cs
+++ microInventario.API/Util/Variables.cs
@@ -43,24 +43,8 @@
                 .AddJsonFile(env)
                 .Build();
 
-            public static string PasswordHash = Environment.GetEnvironmentVariable("TOKEN_PASSWORD_HASH")
-                ?? config.GetSection("AppSettings").GetSection("Token")["PasswordHash"];
-
-            public static string SaltKey = Environment.GetEnvironmentVariable("TOKEN_SALT_KEY")
-                ?? config.GetSection("AppSettings").GetSection("Token")["SaltKey"];
-
-            public static string VIKey = Environment.GetEnvironmentVariable("TOKEN_VI_KEY")
-                ?? config.GetSection("AppSettings").GetSection("Token")["VIKey"];
-
-            public static string Bearer = Environment.GetEnvironmentVariable("TOKEN_BEARER")
-                ?? config.GetSection("AppSettings").GetSection("Token")["Bearer"];
-
             public static string Llave = Environment.GetEnvironmentVariable("TOKEN_KEY")
                 ?? config.GetSection("AppSettings").GetSection("Token")["Llave"];
-
-            public static int Expiration = int.TryParse(Environment.GetEnvironmentVariable("TOKEN_EXPIRATION"), out int exp)
-                ? exp
-                : int.Parse(config.GetSection("AppSettings").GetSection("Token")["Expiration"]);
         }
 
         public static class TipoPersona
